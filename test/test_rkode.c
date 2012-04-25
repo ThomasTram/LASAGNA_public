dd1290991fed7c0d41e31db1bd8a19487c7cd4c7
Thomas <ThomasTram@users.noreply.github.com>
2012-04-25 08:09:16 +0000

    Updated the bessel integration test to facilitate check of refinement option..

--- test/test_rkode.c
+++ test/test_rkode.c
@@ -19,27 +19,62 @@ int main(){
   nu=2.0;
   NU = 2;
   x0 = 1.0;
-  xf = 20.0*nu;
+  xf = 200.0*nu;
   y[0] = jn(NU,x0);
   y[1] = 0.5*(jn(NU-1,x0)-jn(NU+1,x0));
   
   for (i=0; i<tres; i++)
     t_vec[i] = x0+i*(xf-x0)/(tres-1);
+int evolver_ndf15(
+	int (*derivs)(double x,double * y,double * dy,
+		void * parameters_and_workspace, ErrorMsg error_message),
+	double x_ini,
+	double x_final,
+	double * y_inout, 
+ 	int * used_in_output,
+	int neq, 
+	void * parameters_and_workspace_for_derivs,
+	double rtol, 
+	double abstol, 
+	double * t_vec, 
+	int t_res,
+	int *Ap,
+	int *Ai,
+	int (*output)(double x,
+		      double y[],
+		      double dy[],
+		      int index_x,
+		      void * parameters_and_workspace,
+		      ErrorMsg error_message),
+	int (*print_variables)(double x, 
+			       double y[], 
+			       double dy[], 
+			       void *parameters_and_workspace,
+			       ErrorMsg error_message),
+	int (*stop_function)(double x, 
+			     double y[], 
+			     double dy[], 
+			     void *parameters_and_workspace,
+			     ErrorMsg error_message),
+	ErrorMsg error_message);
 
   evolver_radau5(besderivs,
-		    x0,
-		    xf,
-		    y,
-		    useless,
-		    2,
-		    &nu,
-		    1e-4,
-		    1e-15,
-		    t_vec,
-		    tres,
-		    besoutput,
-		    NULL,
-		    error_message);
+		 x0,
+		 xf,
+		 y,
+		 useless,
+		 2,
+		 &nu,
+		 1e-4,
+		 1e-15,
+		 t_vec,
+		 tres,
+		 NULL,
+		 NULL,
+		 besoutput,
+		 NULL,
+		 NULL,
+		 error_message);
   return _SUCCESS_;
 }
 
@@ -49,7 +84,7 @@ int besoutput(double t,
 	      int index_t, 
 	      void *param, 
 	      ErrorMsg error_message){
-  printf("%.12e %.12e %.12e\n",t,y[0],y[1]);
+  fprintf(stderr,"%.12e %.12e %.12e\n",t,y[0],y[1]);
   return _SUCCESS_;
 }
 
